784a9cc0a3b0797e6a7702fb722efea9e7acbff3
Qurban <qurban_ali36@yahoo.com>
2015-07-06 14:20:28 +0500

    initial commit

---
+++ src/_add.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 6, 2015
+
+@author: qurban.ali
+'''
+import pymel.core as pc
+
+def add():
+    sel = pc.ls(sl=True)
+    if not sel:
+        pc.warning('No selection found in the scene')
+        return
+    minTime = pc.playbackOptions(minTime=True, q=True)
+    maxTime = pc.playbackOptions(maxTime=True, q=True)
+    
+    for obj in sel:
+        try:
+            pc.select(obj)
+            pc.mel.eval('setKeyframe -breakdown 0 -hierarchy none -controlPoints 0 -shape 0 -time %s {"%s"};'%(minTime,obj.name()))
+            pc.mel.eval('setKeyframe -breakdown 0 -hierarchy none -controlPoints 0 -shape 0 -time %s {"%s"};'%(maxTime,obj.name()))
+        except Exception as ex:
+            pc.warning(str(ex))
+    
+    
